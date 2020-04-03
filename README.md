be1a351b0690cd177f63daa4e8098e9562878c0e
Jacek Palczewski <jpalczewski@gmail.com>
2020-04-04 01:02:42 +0200

    enable scss, closes #2 and introduces #4

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 
 Nothing, yet. Just another weird side-project and motivation to meet with typescrypt&nuxt&etc.
 In future it might be a nice looking wrapper that manages and helps im printing labels with ql700. A 2020 glabel alternative.
+
+# Recommended settings
+
+For time of committing this word:
+ - project is hardwired with ql-700 linux based
+ - 62mm cont. label
+ - minimum margin settings while printing
--- pages/print/_id.vue
+++ pages/print/_id.vue
@@ -1,24 +1,39 @@
 <template>
-  <section>
-     {{this.$route.params.id }}
-  </section>
+  <div id="sheet">
+    <!--    <p class="title">{{this.$route.params.id }}</p>-->
+    <p class="title">Disgustingly long sample text</p>
+    <p class="subtitle">Awkwardly bottom text</p>
+    <a id="back">Back</a>
+  </div>
 </template>
 
 <script lang="ts">
-  import {
-    Component,
-    Vue
-  } from "nuxt-property-decorator"
+  import { Component, Vue } from 'nuxt-property-decorator'
 
   @Component
   export default class extends Vue {
 
-    get layout() : string {
-      return  "printable"
+    get layout(): string {
+      return 'printable'
     }
   }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  @media print {
+    #back {
+      display: none;
+    }
+    @page {
+      size: 29mm 62mm;
+      margin: 0 0 0 0;
+    }
+    html {
+      width: 62mm;
+      margin: 0;
+      padding: 0;
+      max-width: 62mm;
+      min-width: 62mm;
+    }
+  }
 </style>
